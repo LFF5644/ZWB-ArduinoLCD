bd81267e7c5f51ed1988d7c1824410042635134c
LFF-VivoBook <meerschweinchen5644@gmail.com>
2023-04-27 15:13:07 +0200

    optimised, use IRremote switch

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <LiquidCrystal.h>
 #include <IRremote.hpp>
 
-const int 
+const unsigned char 
 	rs=12,	// Digital 12
 	en=11,	// Digital 11
 	d4=5,	// Digital 05
@@ -12,17 +12,18 @@ const int
 	PIN_button_backLight=6,	// Digital 06
 	PIN_backLight=8,		// Digital 08
 	PIN_irRemote=7,
-	PIN_sensorGas=A0;		// Analog 00
+	PIN_sensorGas=A6;		// Analog 00
 
-const long
+const unsigned long
 	backLight_turnOffTime=1e5+2e4;	// 120s
 
 bool backLight=true;
-long backLight_turnOff=millis()+backLight_turnOffTime;
+unsigned long backLight_turnOff=millis()+backLight_turnOffTime;
 int sensorGas_value;
 int sensorGas_lastValue;
-long delay_1s;
-long delay_500ms;
+unsigned long delay_1s;
+unsigned long delay_500ms;
+long BUTTON_OFF=-1169817856;
 
 LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
 
@@ -31,7 +32,22 @@ void clearLine(int line){
 	lcd.print("                "); // clear all 16 columns
 	lcd.setCursor(0,line);
 }
-void runEvery_500ms(long ms){
+void changeBackLightBool(unsigned char newMode=2){
+	if(newMode==2){
+		if(backLight){newMode=0;}
+		else if(!backLight){newMode=1;}
+	}
+	
+	if(newMode==1){
+		backLight=true;
+		backLight_turnOff=millis()+backLight_turnOffTime;
+		digitalWrite(PIN_backLight,HIGH);
+	}
+	else if(newMode==0){
+		backLight_turnOff=0;
+	}
+}
+void runEvery_500ms(unsigned long ms){
 	if(
 		ms<backLight_turnOff&&
 		backLight
@@ -42,7 +58,7 @@ void runEvery_500ms(long ms){
 		lcd.print("s");
 	}
 }
-void runEvery_1s(long ms){
+void runEvery_1s(unsigned long ms){
 	sensorGas_value=analogRead(PIN_sensorGas);
 	if(sensorGas_value!=sensorGas_lastValue){
 		sensorGas_lastValue=sensorGas_value;
@@ -52,11 +68,12 @@ void runEvery_1s(long ms){
 	}
 }
 
+
 void setup(){
 	pinMode(PIN_backLight,OUTPUT);
 	pinMode(PIN_button_backLight,INPUT);
 	pinMode(PIN_sensorGas,INPUT);
-	IrReceiver.begin(PIN_irRemote, ENABLE_LED_FEEDBACK);
+	IrReceiver.begin(PIN_irRemote, DISABLE_LED_FEEDBACK);
 	digitalWrite(PIN_backLight,HIGH);
 
 	Serial.begin(9600);
@@ -65,7 +82,7 @@ void setup(){
 }
 
 void loop(){
-	const long ms=millis();
+	const unsigned long ms=millis();
 
 	if(ms>delay_1s){
 		delay_1s=ms+1e3;
@@ -83,16 +100,7 @@ void loop(){
 		while(digitalRead(PIN_button_backLight)){}
 		Serial.print(millis()-startPress);
 		Serial.print("\n");
-		if(backLight){
-			Serial.println("Backlight turnt off by button");
-			backLight_turnOff=0;
-		}
-		else if(!backLight){
-			Serial.println("Backlight turnt on by button");
-			backLight=true;
-			backLight_turnOff=ms+backLight_turnOffTime ;
-			digitalWrite(PIN_backLight,HIGH);
-		}
+		changeBackLightBool();
 	}
 
 	if(
@@ -108,12 +116,11 @@ void loop(){
 		long irReceived=IrReceiver.decodedIRData.decodedRawData;
 		if(irReceived!=0){
 			Serial.println(irReceived);
-			if(irReceived==-1169817856){	// if power off btn pressed
-				Serial.println("off");
-				backLight_turnOff=0;	
+			if(irReceived==BUTTON_OFF){	// if power off btn pressed
+				Serial.println("OFF BTN PRESSED!");
+				changeBackLightBool();
 			}
 		}
-		
 		IrReceiver.resume();
 	}
 }
